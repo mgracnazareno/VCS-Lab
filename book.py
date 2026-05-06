4eae40acae3a6b000b6d2668f475694832f0abef
Mary Grace Nazareno <mgracenazareno@gmail.com>
2026-05-06 10:54:05 -0400

    Add book borrow and display_info methods

--- book.py
+++ book.py
@@ -13,6 +13,14 @@ class Book:
         else:
             self.available = False
             print(f"{self.title} has been borrowed.")
-    
 
+    def return_book(self):
+        if(self.available == False):
+            self.available = True
+            print(f"{self.title} has been returned.")
+        else:
+            print(f"{self.title} is not borrowed")
+    
+    def display_info(self):
+        print(f"{self.title} availability: {self.available}")
         
