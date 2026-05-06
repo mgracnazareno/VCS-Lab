7fad9600ba269d691b833e2e7ad1e31d7518e944
Mary Grace Nazareno <mgracenazareno@gmail.com>
2026-05-06 11:01:32 -0400

    Add class variable and method

--- book.py
+++ book.py
@@ -1,10 +1,14 @@
 
 class Book:
+    library_name = "Central Library"
 
     def __init__(self, title, author, available):
         self.title = title
         self.author = author
         self.available = True
     
-       
+    @classmethod
+    def change_library_new(cls, new_name):
+        cls.library_name = new_name
+        
         
