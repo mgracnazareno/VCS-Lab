d0fa4c0ba56803115fe4b9a11debcf29c9dfa27b
Mary Grace Nazareno <mgracenazareno@gmail.com>
2026-05-06 10:46:57 -0400

    Add borrow instance method

--- book.py
+++ book.py
@@ -6,5 +6,13 @@ class Book:
         self.author = author
         self.available = True
     
-       
+    # instance methods
+    def borrow(self):
+        if(self.available == True):
+            print(f"{self.title} Borrowed! ")
+        else:
+            self.available = False
+            print(f"{self.title} has been borrowed.")
+    
+
         
